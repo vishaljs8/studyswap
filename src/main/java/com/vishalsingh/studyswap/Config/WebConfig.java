d438069abe870fec1ad1163bc5c61dfad4ec0cae
vishal singh <vishalbalipur1212@gmail.com>
2025-10-26 19:55:29 +0530

    ready

--- src/main/java/com/vishalsingh/studyswap/Config/WebConfig.java
+++ src/main/java/com/vishalsingh/studyswap/Config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // all endpoints
-                        .allowedOrigins("http://localhost:5173") // your frontend
+                        .allowedOrigins("https://kart-wxpg.onrender.com") // your frontend
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("Authorization", "Content-Type", "Accept")
                         .allowCredentials(true); 
